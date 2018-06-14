320bc7c8ed2ac3ca60413350dac8ca848df0c0db
Andrew <andrew.cherniavskii@gmail.com>
2018-06-14 11:09:22 +0200

    seal defaults object after test

--- test/specs/core/dispatchRequest.spec.js
+++ test/specs/core/dispatchRequest.spec.js
@@ -2,9 +2,15 @@ var defaults = require('../../../lib/defaults');
 var dispatchRequest = require('../../../lib/core/dispatchRequest');
 
 describe('core::dispatchRequest', function() {
-  it('should not mutate config', function() {
+  beforeAll(function() {
     Object.freeze(defaults);
+  });
 
+  afterAll(function() {
+    Object.seal(defaults);
+  });
+
+  it('should not mutate config', function() {
     expect(function() {
       dispatchRequest(defaults)
     }).not.toThrowError(TypeError, /Cannot assign to read only property|is read-only/);
